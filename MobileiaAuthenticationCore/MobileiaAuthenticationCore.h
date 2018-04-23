3e541b0ad115b72e9958ddde67b7907d323d1841
Matias Camiletti <matias.camiletti@gmail.com>
2018-04-23 19:20:51 -0300

    Initial Commit

---
+++ MobileiaAuthenticationCore/MobileiaAuthenticationCore.h
@@ -0,0 +1,19 @@
+//
+//  MobileiaAuthenticationCore.h
+//  MobileiaAuthenticationCore
+//
+//  Created by Matias Camiletti on 23/4/18.
+//  Copyright © 2018 Matias Camiletti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobileiaAuthenticationCore.
+FOUNDATION_EXPORT double MobileiaAuthenticationCoreVersionNumber;
+
+//! Project version string for MobileiaAuthenticationCore.
+FOUNDATION_EXPORT const unsigned char MobileiaAuthenticationCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileiaAuthenticationCore/PublicHeader.h>
+
+
